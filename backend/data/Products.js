e11de77342b6182261962faeb0fda44e570cc041
anantasingh-data-design <anantkumarsingh05@gmail.com>
2021-06-17 21:51:16 +0530

    Ecommerce project submitted

---
+++ backend/data/Products.js
@@ -0,0 +1,97 @@
+const products = [
+  {
+    name: "Hip Hop Boy Elastic  Pant ",
+    imageUrl:
+      "https://www.dhresource.com/260x260s/f2-albu-g18-M00-12-C9-rBNaNmAxA6uAXnBtAAIc8pfj4ko289.jpg/hip-hop-boy-elastic-waist-design-harem-pant.jpg",
+    description:
+      "Hip Hop Boy Elastic Waist Design Harem Pant Men 2021 New Summer Streetwear Punk Casual Trousers Jogger Male Loose Pants #Pi2U .",
+    price: 499,
+    countInStock: 15,
+  },
+  {
+    name: "Boy Formal Wear ",
+    imageUrl:
+      "https://www.dhresource.com/0x0/f2/albu/g7/M00/D1/57/rBVaSltGqtaADQSLAADDuk_nNI4434.jpg/new-design-notch-lapel-black-boy-formal-wear.jpg",
+    description:
+      "New Design Notch Lapel Black Boy Formal Wear Handsome Boy Kid Attire Wedding Wear Birthday Party Prom Suit(jacket+pants+tie+vest ) 26 .",
+    price: 410,
+    countInStock: 10,
+  },
+  {
+    name: "Cannon EOS-1D",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
+    price: 1300,
+    countInStock: 5,
+  },
+  {
+    name: "Amazon Alexa",
+    imageUrl:
+      "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+    description:
+      "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
+    price: 50,
+    countInStock: 25,
+  },
+  {
+    name: "Audio Technica Headphones",
+    imageUrl:
+      "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "Presbyopic and Ms Backpack Laptop",
+    imageUrl:
+      "https://www.dhresource.com/0x0/f2/albu/g18/M01/1F/13/rBNaNmA6bAiARDEvAAPEC0vdXfM638.jpg/2021-general-presbyopic-and-ms-backpack-laptop.jpg",
+    description:
+      "2021 General Presbyopic and Ms Backpack Laptop Quality Men S and Women Unisex Duffel School Bags for Teenage Girls Duffle Bag Toteg Handbag.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "14 inch Laptop",
+    imageUrl:
+      "https://www.dhresource.com/0x0/f2/albu/g17/M01/9E/37/rBVa4V_9rXGAWf8uAAUjNPW4xfs817.jpg/14inch-laptops-wifi-6gb-ram-for-students.jpg",
+    description:
+      "free 14inch laptops wifi 4GB RAM 64G ROM students online class laptops business Windows 10pro pc 128GB/256GB/512GB laptop SSD WIN10 shipping.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "Bags MONTAIGNE",
+    imageUrl:
+      "https://www.dhresource.com/0x0/f2/albu/g16/M01/FA/79/rBVa31-Piu-AM92AAACdnAPYvts891.jpg/newset-bags-montaigne-tote-women-leather.jpg",
+    description:
+      "Newset Bags MONTAIGNE Tote Women Leather Shoulder Bags purse Floral Print Handbags Crossbody big shopper Bag Business Laptop bag.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "Bags MONTAIGNE",
+    imageUrl:
+      "https://www.dhresource.com/0x0/f2/albu/g16/M01/FA/79/rBVa31-Piu-AM92AAACdnAPYvts891.jpg/newset-bags-montaigne-tote-women-leather.jpg",
+    description:
+      "Newset Bags MONTAIGNE Tote Women Leather Shoulder Bags purse Floral Print Handbags Crossbody big shopper Bag Business Laptop bag.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "10W Qi Car Wireless Charger",
+    imageUrl:
+      "https://www.dhresource.com/0x0/f2/albu/g16/M00/72/F7/rBVa4F-Oaw6APVlKAACQRUkMYe4944.jpg/10w-qi-car-wireless-charger-fast-charging.jpg",
+    description:
+      "10W Qi Car Wireless Charger Fast Charging Pad Dock Stand for IPhone 11 Pro Max Samsung Huawei P30 Smart Automatic Sensor.",
+    price: 233,
+    countInStock: 4,
+  },
+  
+  
+
+];
+
+module.exports = products;
